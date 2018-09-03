26b77a779df0601e86fdb3e7e779494ff0e32a8d
larenelg <larene@dccjobs.com>
2018-09-03 23:36:45 +1000

    added some navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <main>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </main>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img className="App-logo" src={logo} alt="" />
+            <Link to="/"><h1>A11y PR</h1></Link>
+            <nav className="App-nav">
+              <ul>
+                <li><Link to="/list">List</Link></li>
+                <li><Link to="/add">Add</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            <Route path="/" exact={true} component={() => <p>Let's fix some things!</p>} />
+            <Route path="/list" component={List} />
+            <Route path="/add" component={Add} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
+
+const List = () => <h2>List</h2>;
+const Add = () => <h2>Add</h2>;
 
 export default App;
